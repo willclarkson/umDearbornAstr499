c11b13fcbf4c7a5102eb3d39d6cc1be72259c51a
zseblini <zseblini@sweet.natsci.umd.umich.edu>
2019-12-13 13:07:20 -0500

    gathering statistics into a table

--- python/zseblini/program_dy_2/doAnim.py
+++ python/zseblini/program_dy_2/doAnim.py
@@ -7,14 +7,20 @@
 import programday_4
 import glob
 import os
+from astropy.table import Table, vstack
 
 def doMany(srchString='Comet_R',catTail='ASC', Verbose=True):
 
     """Loops through the .asc catalogs and runs the programday4"""
 
     # first, find the .asc catalogs
-    lCats = glob.glob('%s*.%s' % (srchString, catTail))
+    pattn='%s*.%s' % (srchString, catTail)
+    lCats = glob.glob(pattn)
+    MasterTable=Table()
 
+    if len(lCats) < 1:
+        print("doAnim.doMany WARN - no files match search string %s" % (pattn))
+    
     # now run programday4 on each file.
     for iCat in range(len(lCats)):
 
@@ -25,10 +31,17 @@ def doMany(srchString='Comet_R',catTail='ASC', Verbose=True):
         thisImg = '%s.fits' % (stemCat)
 
         if not os.access(thisImg, os.R_OK):
-            print("doAnim.doMany WARN - cannot read image file %s" \
-                  % (thisImg))
-            return
+            if Verbose:
+                print("doAnim.doMany WARN - cannot read image file %s" \
+                      % (thisImg))
+            continue
 
         # now we're here, we can run "go"
-        programday_4.go(thisCat, thisImg)
+        ThisTable=programday_4.go(thisCat, thisImg)
+        MasterTable=vstack([MasterTable,ThisTable])
+        #print(ThisTable)
+        # stack the table
         
+        
+        #os.system("ffmpeg -r 1 -i *.jpg -vcodec mpeg4 -y movie1.mp4")
+    print(MasterTable)
